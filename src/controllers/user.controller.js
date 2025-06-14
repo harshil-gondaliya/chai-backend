7c8cc00779ba2787fdf07d49592402d8764fc22b
Harshil Gondaliya <gondaliyaharshil555@gmail.com>
2025-06-14 15:05:05 +0530

    add get watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -442,6 +442,56 @@ const getUserChannelProfile = asyncHandler(async(requestAnimationFrame,res) => {
     
 })
 
+const getWatchHistory = asyncHandler(async(req,res) => {
+  const user = await User.aggregate([
+    {
+      $match :{
+        _id : new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup : {
+        from : "videos" ,
+        localField : "watchHistory",
+        foreignField : "_id",
+        as: "watchHistory",
+        pipeline : [
+          {
+            $lookup : {
+              from : "users",
+              localField : "owner",
+              foreignField : "_id",
+              as : "owner",
+              pipeline : [
+                {
+                  $project : {
+                    fullname :1 ,
+                    username :1 ,
+                    avatar :1 ,
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner :{
+                $first :"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+
+  ])
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,user[0].watchHistory ,"watch history fatch successfully")
+  )
+  
+})
 
 export {
   registerUser,
@@ -454,4 +504,5 @@ updateAccountDetails,
 updateUserAvatar,
 updateUserCoverImage,
 getUserChannelProfile,
+getWatchHistory,
 }
